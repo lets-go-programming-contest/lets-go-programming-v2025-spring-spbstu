a84c2ab88f94175daf901012be98689163948e31
dimonrum003 <drumyantsevy@gmail.com>
2025-02-25 02:15:33 +0300

    [TASK-2-2] Initial commit

---
+++ dmitriy.rumyantsev/task-2-2/pkg/min_heap/min_heap.go
@@ -0,0 +1,36 @@
+package min_heap
+
+import "container/heap"
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] } // min-heap
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func findKthLargest(nums []int, K int) int {
+	h := &IntHeap{}
+	heap.Init(h)
+
+	for _, num := range nums {
+		heap.Push(h, num)
+	}
+
+	for h.Len() > K {
+		heap.Pop(h)
+	}
+
+	return (*h)[0]
+}
