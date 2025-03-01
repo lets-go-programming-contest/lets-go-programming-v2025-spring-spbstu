1e8b136a609b4ed8c59ba7890b0ace955a9c2003
sovus_tartar <vad-nesmejanov@yandex.ru>
2025-03-01 20:24:01 +0300

    [Task-3] attempt to read xml

--- vadim.nesmeianov/task-3/cmd/service/main.go
+++ vadim.nesmeianov/task-3/cmd/service/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"log"
+	"os"
 
+	"golang.org/x/net/html/charset"
+)
+
+type ValCurs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Text    string   `xml:",chardata"`
+	Date    string   `xml:"Date,attr"`
+	Name    string   `xml:"name,attr"`
+	Valute  []struct {
+		Text      string `xml:",chardata"`
+		ID        string `xml:"ID,attr"`
+		NumCode   string `xml:"NumCode"`
+		CharCode  string `xml:"CharCode"`
+		Nominal   string `xml:"Nominal"`
+		Name      string `xml:"Name"`
+		Value     string `xml:"Value"`
+		VunitRate string `xml:"VunitRate"`
+	} `xml:"Valute"`
+}
+
+func main() {
+	filePath := "example_data/1.xml"
+
+	// Read the file as a byte string
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := bytes.NewReader([]byte(content))
+	d := xml.NewDecoder(r)
+	d.CharsetReader = charset.NewReaderLabel
+
+	valCurs := new(ValCurs)
+	err = d.Decode(&valCurs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(valCurs)
 }
