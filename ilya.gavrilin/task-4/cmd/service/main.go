010bec6f2e225ec7d3cb1f6446515cfc424cd0d3
Ilya Gavrilin <gavrilinid@gmail.com>
2025-03-11 18:00:54 +0300

    [TASK-4] feat: fixed data race in http server setup

--- ilya.gavrilin/task-4/cmd/service/main.go
+++ ilya.gavrilin/task-4/cmd/service/main.go
@@ -3,42 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
 
 	"task-4/internal/cache"
 	"task-4/internal/printer"
 	"task-4/internal/reader"
 )
 
-const useSync = true // Enable/Disable sync
+const useSync = false // Enable/Disable sync
 
 func main() {
 	c := cache.NewCache(useSync)
-	var wg sync.WaitGroup
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
+		url := reader.GenerateRandomURL()
 
-			url := reader.GenerateRandomURL()
+		// Check if data is cached
+		if data, found := c.Get(url); found {
+			printer.LogRequest(url, data, true)
+			fmt.Fprintf(w, "Cached Response: %s\n", data)
+			return
+		}
 
-			// check if data cached
-			if data, found := c.Get(url); found {
-				printer.LogRequest(url, data, true)
-				fmt.Fprintf(w, "Cached Response: %s\n", data)
-				return
-			}
-
-			// if not in cache: emulate "request" and set
-			data := cache.SimulateExternalFetch(url)
-			c.Set(url, data)
-			printer.LogRequest(url, data, false)
-			fmt.Fprintf(w, "Fetched Response: %s\n", data)
-		}()
+		// If not in cache: fetch and store
+		data := cache.SimulateExternalFetch(url)
+		c.Set(url, data)
+		printer.LogRequest(url, data, false)
+		fmt.Fprintf(w, "Fetched Response: %s\n", data)
 	})
 
 	fmt.Println("Starting server on :8080")
+	//it always returns non-nil errror, so leave it unchecked
+	// Used ionly as primitive demo, not use in real project without CLose
 	http.ListenAndServe(":8080", nil)
-	wg.Wait()
 }
