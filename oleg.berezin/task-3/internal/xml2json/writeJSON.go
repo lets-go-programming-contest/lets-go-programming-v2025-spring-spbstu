11497f2c5d78f3d33744e17f60e443f01348115d
Oleg Berezin <olegberezin2002@gmail.com>
2025-03-18 15:50:06 +0300

    [task-3] Add writeJSON function

--- oleg.berezin/task-3/internal/xml2json/writeJSON.go
+++ oleg.berezin/task-3/internal/xml2json/writeJSON.go
@@ -1,1 +1,12 @@
 package xml2json
+
+import "encoding/json"
+
+func writeJSON(valCurs Format) []byte {
+	jsonData, err := json.Marshal(valCurs)
+	if err != nil {
+		panic("Error during marshal json")
+	}
+
+	return jsonData
+}
