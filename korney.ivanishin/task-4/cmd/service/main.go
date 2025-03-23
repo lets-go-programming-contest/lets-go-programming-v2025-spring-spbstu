f5268472c794ac2d3611cc5b8ba03ac36e542e47
Korney Ivanishin <korney059@gmail.com>
2025-03-23 22:02:56 +0300

    [TASK-4] Replace always panicing routines with normal errgroup-powered error handling

--- korney.ivanishin/task-4/cmd/service/main.go
+++ korney.ivanishin/task-4/cmd/service/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 
 	"github.com/quaiion/go-practice/lru-cache/internal/CacheDir"
 	"github.com/quaiion/go-practice/lru-cache/internal/Config"
 	"github.com/quaiion/go-practice/lru-cache/internal/Requester"
+	"golang.org/x/sync/errgroup"
 )
 
 var (
         errConfigFailed = errors.New("failed extracting config parameters")
+        errRequestSimFailed = errors.New("failed simulating requests")
 )
 
 func main() {
@@ -22,11 +25,15 @@ func main() {
         cacheDir := CacheDir.NewCacheDir(configParams.ReqRange,
                                          configParams.CacheCap)
 
-        chanArr := launchRequesters(&cacheDir, configParams.NRequesters, 
-                                               configParams.ReqRange,
-                                               configParams.NRequests)
+        chanArr, group := launchRequesters(&cacheDir,
+                                           configParams.NRequesters, 
+                                           configParams.ReqRange,
+                                           configParams.NRequests)
 
-        hitSum := joinRequesters(chanArr, configParams.NRequesters)
+        hitSum, err := joinRequesters(chanArr, configParams.NRequesters, group)
+        if err != nil {
+                panic(errors.Join(errRequestSimFailed, err))
+        }
 
         fmt.Printf("\nhit rate: %d/%d, %.2f%%\n\n", hitSum,
                    configParams.NRequests * configParams.NRequesters,
@@ -35,33 +42,50 @@ func main() {
                                      float64(100))
 }
 
-func goroutRequestN(requester Requester.Requester, cd *CacheDir.CacheDir, n uint32,  ch chan<- uint32) {
+func goroutRequestN(requester Requester.Requester, cd *CacheDir.CacheDir, n uint32, ch chan<- uint32) error {
         nHits, err := requester.RequestN(cd, n)
         if err != nil {
-                panic(err)
+                // without this the system deadlocks waiting
+                // for the channel in an error scenario
+                ch <- 0
+
+                return err
         }
 
         ch <- nHits
+        return nil
 }
 
-func launchRequesters(cd *CacheDir.CacheDir, nRequesters uint32, reqRange uint32, nRequests uint32) *[]chan uint32 {
+func launchRequesters(cd *CacheDir.CacheDir, nRequesters uint32, reqRange uint32, nRequests uint32) (*[]chan uint32, *errgroup.Group) {
         chanArr := make([]chan uint32, 0, nRequesters)
+
+        group, _ := errgroup.WithContext(context.Background())
 
         for i := uint32(0) ; i < nRequesters ; i += 1 {
                 requester := Requester.NewRequester(reqRange)
                 chanArr = append(chanArr, make(chan uint32))
-                go goroutRequestN(requester, cd, nRequests, chanArr[i])
+                group.Go(func() error {
+                        return goroutRequestN(requester, cd, nRequests, chanArr[i])
+                })
         }
 
-        return &chanArr
+        return &chanArr, group
 }
 
-func joinRequesters(chanArr *[]chan uint32, nRequesters uint32) uint32 {
-        var hitSum uint32 = 0
+var errRequestFailed = errors.New("failed creating or processing a request")
 
+func joinRequesters(chanArr *[]chan uint32, nRequesters uint32, group *errgroup.Group) (uint32, error) {
+        // postponed summation not required as zero is
+        // returned from goroutine in an error case
+        var hitSum uint32 = 0
         for i := uint32(0) ; i < nRequesters ; i += 1 {
                 hitSum += <-((*chanArr)[i])
         }
+        
+        err := group.Wait()
+        if err != nil {
+                return 0, errors.Join(errRequestFailed, err)
+        }
 
-        return hitSum
+        return hitSum, nil
 }
