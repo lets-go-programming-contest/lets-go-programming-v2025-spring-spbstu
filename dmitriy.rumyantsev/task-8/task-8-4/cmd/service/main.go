62bd13828c449c5c419ae81e2b9e87eee6450419
RumyantsevDmitriy <rumiantsev.da@phystech.edu>
2025-04-08 19:41:18 +0300

    [TASK-8] Fix generate

--- dmitriy.rumyantsev/task-8/task-8-4/cmd/service/main.go
+++ dmitriy.rumyantsev/task-8/task-8-4/cmd/service/main.go
@@ -8,15 +8,13 @@ import (
 	"go.uber.org/mock/gomock"
 )
 
-//go:generate go generate ../../internal/user/user.go
-
 func main() {
 	ctrl := gomock.NewController(nil)
-    defer ctrl.Finish()
+	defer ctrl.Finish()
 
-    mock := mocks.NewMockUser(ctrl)
-    mock.EXPECT().GetUser(1).Return("Dima", nil)
+	mock := mocks.NewMockUser(ctrl)
+	mock.EXPECT().GetUser(1).Return("Dima", nil)
 
-    name, _ := mock.GetUser(1)
-    fmt.Println("User:", name)
+	name, _ := mock.GetUser(1)
+	fmt.Println("User:", name)
 }
