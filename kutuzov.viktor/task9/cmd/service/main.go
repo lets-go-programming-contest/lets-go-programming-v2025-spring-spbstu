a7a1ac8529422b6a383480a5002415cc73077cb4
vktr-ktzv <kutuzov.vm@phystech.edu>
2025-04-20 19:51:38 +0300

    [TASK-9] Перенес настройку роутера в отдельную функцию

--- kutuzov.viktor/task9/cmd/service/main.go
+++ kutuzov.viktor/task9/cmd/service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
@@ -9,10 +10,7 @@ import (
 	contactdatabase "github.com/vktr-ktzv/contact-api/internal/contactDatabase"
 )
 
-func main() {
-	db := contactdatabase.ConnectDB()
-	defer db.Close()
-
+func getRouter(db *sql.DB) *mux.Router {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/contacts", contactdatabase.GetContacts(db)).Methods("GET")
@@ -20,6 +18,15 @@ func main() {
 	router.HandleFunc("/contacts", contactdatabase.CreateContact(db)).Methods("POST")
 	router.HandleFunc("/contacts/{id}", contactdatabase.UpdateContact(db)).Methods("PUT")
 	router.HandleFunc("/contacts/{id}", contactdatabase.DeleteContact(db)).Methods("DELETE")
+
+	return router
+}
+
+func main() {
+	db := contactdatabase.ConnectDB()
+	defer db.Close()
+
+	router := getRouter(db)
 
 	log.Println("Server started on :8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
