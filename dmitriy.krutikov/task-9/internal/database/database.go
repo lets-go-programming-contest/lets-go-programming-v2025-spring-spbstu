9e171b782fd0c8961075447df62adc93a382576b
dik-sc <dmitriy.krutikov@syntacore.com>
2025-04-22 18:15:24 +0300

    [TASK-9] add task-9

---
+++ dmitriy.krutikov/task-9/internal/database/database.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "root"
+	dbname   = "phonebook"
+)
+
+func InitDB() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS contacts (
+			id SERIAL PRIMARY KEY,
+			name TEXT NOT NULL,
+			phone TEXT NOT NULL UNIQUE
+		)
+	`)
+	
+	if err != nil {
+		return nil, fmt.Errorf("failed to create table: %v", err)
+	}
+
+	return db, nil
+}
