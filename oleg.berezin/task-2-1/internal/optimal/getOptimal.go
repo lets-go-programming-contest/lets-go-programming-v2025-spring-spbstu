25ab3be6cf4d128b3802d19b13a3613cb332513d
Oleg Berezin <olegberezin2002@gmail.com>
2025-05-18 17:24:27 +0300

    [task-2-1] Add intersection case

--- oleg.berezin/task-2-1/internal/optimal/getOptimal.go
+++ oleg.berezin/task-2-1/internal/optimal/getOptimal.go
@@ -1,9 +1,19 @@
 package optimal
 
-import "task-2-1/internal/temperature"
+import (
+	"task-2-1/internal/temperature"
+)
 
 func inInt(interval OptInt, t int) bool {
 	return !(t < interval.T1 || t > interval.T2)
+}
+
+func isIntersected(inteval1 OptInt, t temperature.Temperature) bool {
+	if t.Less {
+		return inteval1.T2 < t.T
+	} else {
+		return inteval1.T2 > t.T
+	}
 }
 
 func GetOptInt(interval OptInt, t temperature.Temperature) OptInt {
@@ -13,6 +23,8 @@ func GetOptInt(interval OptInt, t temperature.Temperature) OptInt {
 		} else {
 			return OptInt{t.T, interval.T2}
 		}
+	} else if isIntersected(interval, t) {
+		return interval
 	} else {
 		return OptInt{-1, -1}
 	}
