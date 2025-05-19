56b2041d929d56d75036e9aabd5b827767fe9a20
Oleg Berezin <olegberezin2002@gmail.com>
2025-05-19 21:42:16 +0300

    [task-2-2] Add intheap

---
+++ oleg.berezin/task-2-2/internal/intheap/intHeap.go
@@ -0,0 +1,36 @@
+package intheap
+
+import "container/heap"
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func GetK(nums []int, k int) int {
+	h := &IntHeap{}
+	heap.Init(h)
+
+	for _, num := range nums {
+		if h.Len() < k {
+			heap.Push(h, num)
+		} else if num > (*h)[0] {
+			heap.Pop(h)
+			heap.Push(h, num)
+		}
+	}
+	return (*h)[0]
+}
