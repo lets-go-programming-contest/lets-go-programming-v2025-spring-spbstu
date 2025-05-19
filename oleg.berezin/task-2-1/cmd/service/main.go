b6ce57d00f87663814daeb85db4595dad5b8eb34
Oleg Berezin <olegberezin2002@gmail.com>
2025-05-19 21:46:54 +0300

    [task-2-1] Add main

--- oleg.berezin/task-2-1/cmd/service/main.go
+++ oleg.berezin/task-2-1/cmd/service/main.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"fmt"
+	"task-2-1/internal/office"
+	"task-2-1/internal/optimal"
+	"task-2-1/internal/temperature"
+)
+
+func main() {
+	Y, err := office.GetOffice()
+	if err != nil {
+		fmt.Printf("error during GetOffice\n")
+		panic(err)
+	}
+
+	for n := 0; n < Y; n++ {
+		n, err := office.GetDepartment()
+		if err != nil {
+			fmt.Printf("error during GetDepartment\n")
+			panic(err)
+		}
+
+		t0, err := temperature.GetTemperature()
+		if err != nil {
+			fmt.Printf("error during GetTemperature\n")
+			panic(err)
+		}
+
+		optInt := optimal.GetOptInt(optimal.OptInt{T1: 15, T2: 30}, t0)
+
+		optimal.ShowOptimal(optInt)
+
+		for i := 1; i < n; i++ {
+			t, err := temperature.GetTemperature()
+			if err != nil {
+				fmt.Printf("error during GetTemperature\n")
+				panic(err)
+			}
+
+			optInt = optimal.GetOptInt(optInt, t)
+
+			optimal.ShowOptimal(optInt)
+		}
+	}
+}
